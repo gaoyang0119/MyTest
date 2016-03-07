bcf18ac76bac6d41d3981668e09760f07cdbcecd
gaoyang0119 <952175488@qq.com>
2016-03-07 16:29:38 +0800

    呃呃呃
    日日日

--- src/main/java/com/gy/test/MyTest.java
+++ src/main/java/com/gy/test/MyTest.java
@@ -2,7 +2,9 @@ package com.gy.test;
 
 public class MyTest {
 	public static void main(String[] args) {
-		System.out.println("ffffddddfffffffffffffffffffffddddf");
-		System.out.println("ffdddcfffffffffffffffffffffffddddf");
+                 反反复复
+		System.out.println("ffffddddffffffffff呃呃呃fffffffffffddddf");
+                System.out.println("ffffddddfffffff得到fff呃呃呃fffffffffffddddf");
+		System.out.println("ffdddcffff得到fffffffffffffffffffddddf");
 	}
 }
